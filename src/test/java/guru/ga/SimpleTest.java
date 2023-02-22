8b63a1bb03fa68744cea5a2055618e149ed8b8de
solo <asolovev@awg.ru>
2023-02-22 19:57:58 +0300

    added assert false example

--- src/test/java/guru/ga/SimpleTest.java
+++ src/test/java/guru/ga/SimpleTest.java
@@ -2,6 +2,7 @@ package guru.ga;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SimpleTest {
@@ -9,5 +10,9 @@ public class SimpleTest {
     @Test
     void assertTrueTest() {
         assertTrue( 3>2);
+    }
+    @Test
+    void assertFalseTest() {
+        assertFalse( 3<2);
     }
 }
